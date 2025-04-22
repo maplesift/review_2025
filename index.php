0781afc41567bb5c1aa920907d220c80f773a97f
maplesift <a23050716@gmail.com>
2025-04-22 15:40:41 +0800

    複習 0422

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>review</title>
+    <link rel="stylesheet" href="./css.css">
+    <style>
+
+    </style>
 </head>
 
 <body>
@@ -51,13 +55,114 @@
         -->
     </h3>
     <?php
-    $y=2024;
-    if(($y % 400==0 )||($y %100 !=0 && $y %4==0 ) ){
+    $y = 2024;
+    if (($y % 400 == 0) || ($y % 100 != 0 && $y % 4 == 0)) {
         echo "$y 為閏年";
-    }else{
+    } else {
         echo "$y 為平年";
     }
     ?>
+    <h3>
+        1.用 PHP 實作一個計算 Fibonacci number 的 Function
+        例如： fib(6) 的結果為 8
+        請用 Recursive 及 非 Recursive 的方式各作一次。
+        fib= 0 1 1 2 3 5 8 13
+    </h3>
+    <?php
+    function fib($n)
+    {
+        if ($n == 0) return 0;
+        if ($n == 1) return 1;
+        return fib($n - 1) + fib($n - 2);
+    }
+    echo fib(6);
+    function fib2($n)
+    {
+        if ($n == 0) return 0;
+        if ($n == 1) return 1;
+        $a = 0;
+        $b = 1;
+        for ($i = 2; $i <= $n; $i++) {
+            $tmp = $a + $b; //8
+            $a = $b; // 5
+            $b = $tmp; //8
+        }
+        return $b;
+    }
+    echo fib2(6);
+    ?>
+    <h3>
+        2.給定一個 PHP array, 例如：data = [ 10, 20, 50, 7, 9,1 ];寫一個函數計算該 array 的中位數。
+    </h3>
+    <?php
+
+    $data = [10, 20, 50, 7, 9,];
+    $data1 = [1, 2, 3, 4, 5, 6];
+    function median_array($data)
+    {
+        // 整理陣列
+        sort($data);
+        // 長度
+        $count = count($data);
+        // 奇
+        if ($count % 2 != 0) {
+            // 地板值
+            return $data[floor($count / 2)];
+        } else {
+            // 偶
+            $mid1 = $data[$count / 2 - 1];
+            $mid2 = $data[$count / 2];
+            return ($mid1 + $mid2) / 2;
+        }
+    }
+    echo median_array($data1);
+    ?>
+    <h3>
+        以表格排列的九九乘法表
+    </h3>
+
+    <table>
+    <?php
+    
+    for ($i=1; $i <= 9; $i++) { 
+        echo "<tr>";
+        for ($y=1; $y <=9; $y++) { 
+            echo "<td class=ninex>";
+            echo  "$i x $y=". $i*$y;
+            echo "</td>";
+        }
+        echo "</tr>";
+        // echo "<br>";
+    }
+    ?>
+    </table>
+    <h3>
+    以交叉計算結果呈現的九九乘法表
+    </h3>
+    <table>
+    <tr>
+        <th></th>
+    <?php
+    for ($i=1; $i <=9 ; $i++) { 
+        echo "<th>$i</th>";
+    }
+    ?>
+    </tr>
+    <?php
+    for ($row=1; $row <=9 ; $row++) { 
+        echo "<tr>";
+        echo "<th>";
+        echo $row;
+        echo "</th>";
+        for ($col=1; $col <=9 ; $col++) { 
+            echo "<td>";
+            echo $col;
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+    </table>
 </body>
 
 </html>
